98edaee9e627eacba0ec571def619e0609dc9faa
Hao Chen <chenh123@uw.edu>
2019-03-10 21:44:49 -0700

    Print hello it's me 10 times

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(){
    cout << "Hello world!" << endl; 
 
-   cout << "Hello it's me" << endl; 
+   for (int i = 0; i < 10; i++){
+       cout << "Hello it's me" << endl; 
+   }
    cout << "Hello from the out side" << endl; 
 }
